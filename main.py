6732357cfa52bbd3d9c0b19949adc0b73d033d3d
aqueleleee <116666891+aqueleleee@users.noreply.github.com>
2023-01-26 00:45:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
+import time as t
 from pygame import *
 
 class Dino(sprite.Sprite):
     def __init__(self):
         super().__init__()
-        
+        self.image1 = transform.scale(image.load('run1.png'), (100, 75))
+        self.image2 = transform.scale(image.load('run2.png'), (100, 75))
+        self.height = 100
+        self.width = 75
+        self.last_update = time.get_ticks()
+    def dinoRun(self):
+        now = time.get_ticks()
+        if now - self.last_update > 100:
+            window.blit(self.image1, (100,300))
+            self.last_update = now
+        else:
+            window.blit(self.image2, (100,300))
+            t.sleep(0.06)
+
+
 window = display.set_mode((700, 500))
 
-background = transform.scale(image.load('background.jpg'), (700, 500))
 
-
-
-
-
-
-FPS = 30
+FPS = 60
 clock = time.Clock()
 
-game = True
-while game:
-    for i in event.get():
-        if i.type == QUIT:
-            game = False
-    window.blit(background, (0, 0))
+
+dino = Dino()
+
+def draw():
+    finish = False
+    game = True
+    while game:
+        for even in event.get():
+            if even.type == QUIT:
+                game = False
 
 
+        window.fill((255, 255, 255))
+        dino.dinoRun()
 
-    clock.tick(FPS)
-    display.update()
+        clock.tick(FPS)
+        display.update()
+
+if __name__ == '__main__':
+    draw()
