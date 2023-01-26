1dfd878a489c010a9ac93d7e6d6f51b4de3d7db4
aqueleleee <116666891+aqueleleee@users.noreply.github.com>
2023-01-26 12:40:12 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,20 +4,45 @@ from pygame import *
 class Dino(sprite.Sprite):
     def __init__(self):
         super().__init__()
+        self.rectY = 300
+        self.rectX = 100
         self.image1 = transform.scale(image.load('run1.png'), (100, 75))
         self.image2 = transform.scale(image.load('run2.png'), (100, 75))
+        self.jumpImg = transform.scale(image.load('jump.png'), (100, 75))
         self.height = 100
         self.width = 75
         self.last_update = time.get_ticks()
-    def dinoRun(self):
-        now = time.get_ticks()
-        if now - self.last_update > 100:
-            window.blit(self.image1, (100,300))
-            self.last_update = now
-        else:
-            window.blit(self.image2, (100,300))
-            t.sleep(0.06)
+        self.speed = 10
+        self.run = True
+        self.hightJump = 200
+        self.roadY = 300
+        self.gr = True
 
+    def dinoRun(self):
+        if self.rectY == 300:
+            now = time.get_ticks()
+            if now - self.last_update > 100:
+                window.blit(self.image1, (self.rectX,self.rectY))
+                self.last_update = now
+            else:
+                window.blit(self.image2, (self.rectX,self.rectY))
+                t.sleep(0.06)
+    def jump(self):
+        if self.rectY > 200:
+            self.rectY -= 10
+            if self.rectY == 200:
+                self.gr = True
+        window.blit(self.jumpImg, (self.rectX, self.rectY))
+
+    def update(self):
+        presseds = key.get_pressed()
+        if presseds[K_UP] or presseds[K_SPACE]:
+            self.jump()
+            self.gr = False
+
+    def gravitation(self):
+        if self.gr and self.rectY <= self.roadY:
+            self.rectY += 10
 
 window = display.set_mode((700, 500))
 
@@ -39,6 +64,8 @@ def draw():
 
         window.fill((255, 255, 255))
         dino.dinoRun()
+        dino.update()
+        dino.gravitation()
 
         clock.tick(FPS)
         display.update()
